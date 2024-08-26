21e16ce77fbfaeab6d7115b11e7c69a6fe1a37c4
Thalys Farias <thalysfarias11@gmail.com>
2024-08-26 04:21:31 -0300

    feat: improve schema

--- src/schemas/person/registerPersonSchema.ts
+++ src/schemas/person/registerPersonSchema.ts
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import { personIdentificationLength } from 'src/consts/person';
+import { validatePersonIdentification } from 'src/utils/functions/person';
 import { z } from 'zod';
 
 export const registerPersonSchema = z.object({
@@ -12,14 +13,19 @@ export const registerPersonSchema = z.object({
       const today = moment();
       const isValidBirthdate = date.isValid() && date.isBefore(today);
       return isValidBirthdate;
-    }, 'Invalid birthdate'),
+    }, 'Data de   aniversário inválida'),
   identification: z
     .string()
     .min(1)
-    .refine(({ length }) => {
+    .refine((identification) => {
       const identificationLengths = Object.values(personIdentificationLength);
-      return identificationLengths.includes(length);
-    }, 'Invalid identification'),
+      const isValidLength = identificationLengths.includes(
+        identification.length,
+      );
+      if (!isValidLength) return false;
+      const { isValid } = validatePersonIdentification(identification);
+      return isValid;
+    }, 'Identificação inválida'),
 });
 
 export type RegisterPersonDto = Required<z.infer<typeof registerPersonSchema>>;
