b8686951fe28212e29aac19ebf20c50764d53638
Thalys Farias <thalysfarias11@gmail.com>
2024-08-26 12:43:55 -0300

    feat: improve cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://emprest-ai-frontend.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  });
   await app.listen(3003);
 }
 bootstrap();
