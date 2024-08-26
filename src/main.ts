6dc3165e4b5147948ff370b3dd4fcc903cf7d280
Thalys Farias <thalysfarias11@gmail.com>
2024-08-26 12:57:01 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://emprest-ai-frontend.vercel.app'],
+    origin: ['*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
