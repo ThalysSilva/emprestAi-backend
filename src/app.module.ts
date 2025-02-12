6c0398d727fe0d84d46c4a240eca846c683d8313
Thalys Farias <thalysfarias11@gmail.com>
2025-02-12 03:36:38 -0300

    fix: cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 import { ExceptionFilterTreatment } from './common/filters/exceptionFilter';
