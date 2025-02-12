bcdf89c303ff3b3b1a9919509d5b2304a67846fc
Thalys Farias <thalysfarias11@gmail.com>
2025-02-12 16:51:58 -0300

    fix: resolvendo problema de build

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import 'module-alias/register';
 
 dotenv.config();
 
