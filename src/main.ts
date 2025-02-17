60a22beb4e48a9295a4d46d6ff794761ca618596
Thalys Farias <thalysfarias11@gmail.com>
2025-02-17 17:57:48 -0300

    chore: corrigindo lint do codigo

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,10 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
-import * as fs from 'fs';
 
 dotenv.config();
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
 
